d82e7a491d8b64a9c92aa88d85744c9081db0221
XRockKimu <kimkheng.ly@student.cadt.edu.kh>
2025-04-01 15:38:35 +0700

    Initial commit for Netlify deployment

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFdTbryWuEXlyaQrUSopFEZX0U8eSLEC8",
-  authDomain: "smart-study-planner-2.firebaseapp.com",
-  projectId: "smart-study-planner-2",
-  storageBucket: "smart-study-planner-2.firebasestorage.app",
-  messagingSenderId: "1022008894822",
-  appId: "1:1022008894822:web:efe7448905bf31dd92d4a7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
